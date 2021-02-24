1d19cf3954f9bc70c32e5e846bf960d9ccc4effc
Reaper Gelera <sidgroks@gmail.com>
2021-02-24 11:48:37 +0530

    add sublime merge and go

--- setup.sh
+++ setup.sh
@@ -2,16 +2,14 @@
 
 
 ### TODO
-## - Go Lang download pkg and run it
-## - Download Sublime Merge and add it to Applications 
 ## - Clone vscode settings from git gist
 
 
 
 # Homebrew 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install git yarn make fastlane ngrok
-brew install clean-me visual-studio-code google-chrome iterm2 docker vlc postgres adoptopenjdk/openjdk/adoptopenjdk8
+brew install git yarn make fastlane ngrok go
+brew install clean-me visual-studio-code google-chrome iterm2 docker vlc postgres sublime-merge adoptopenjdk/openjdk/adoptopenjdk8
 
 # ZSH Setup
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
@@ -20,6 +18,8 @@ echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 
 
 # Languages support 
+echo "export PATH=$PATH:$HOME/go/bin" >> ~/.zshrc
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | zsh
 source ~/.zshrc
 nvm install --lts
